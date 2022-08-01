c6944c7ed6520253cef2f2d1c8ae59bb620f567d
MayuriPawan <mayurib15@gmail.com>
2022-08-01 11:32:50 +0530

    add rough.py

---
+++ rough.py
@@ -0,0 +1,17 @@
+l = [1,2,3,4,5]
+# sum = 0
+# for item in l:
+#     sum += item
+
+# print(sum)
+
+
+
+# for i, num in enumerate(l):
+#     s = num + l[i]+len(l)
+
+# print(l[i+1])
+print(s)
+
+# list = [num + l[i+1] for i, num in enumerate(l)]
+# print(list)
